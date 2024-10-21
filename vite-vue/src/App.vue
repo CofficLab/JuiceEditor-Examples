cb0e7039019efc9f38a73b515115c77989de7aaa
Nookery <nooks@qq.com>
2024-10-21 18:27:18 +0800

    🎨 Chore:  Minor adjustments

--- vite-vue/src/App.vue
+++ vite-vue/src/App.vue
@@ -1,36 +1,20 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import { defineJuiceEditor } from '@coffic/juice-editor'
+import { defineJuiceEditor, EventName, EventType } from '@coffic/juice-editor'
+
+window.addEventListener(EventName, (event: CustomEvent) => {
+  if (event.detail.type == EventType.editorReady) {
+    console.log('editor ready');
+    (window as any).api.node.setHTML('Hello World')
+  }
+})
 
 defineJuiceEditor()
+
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div style="width: 100%; height: 100%; border: 1px solid #ccc; text-align: center;">
+    <juice-editor />
   </div>
-  <HelloWorld msg="Vite + Vue" />
-  <juice-editor />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
